deb8c49b6cc7d37e4d0bbfba6b0b03fcee7cdaa2
Iván López <lopez.ivan@gmail.com>
2013-10-08 12:50:13 +0200

    Add Spock plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,4 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = 'target'
 
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
@@ -31,6 +29,10 @@ grails.project.dependency.resolution = {
         build(":tomcat:$grailsVersion",
               ":release:2.2.0",
               ":rest-client-builder:1.0.3") {
+            export = false
+        }
+
+        test ":spock:0.7", {
             export = false
         }
     }
