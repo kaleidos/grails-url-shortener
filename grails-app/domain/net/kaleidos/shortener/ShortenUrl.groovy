b4ffb6b916878aacc7c839be8998cc91733f9f31
Iván López <lopez.ivan@gmail.com>
2013-10-08 15:20:21 +0200

    Add domain class

---
+++ grails-app/domain/net/kaleidos/shortener/ShortenUrl.groovy
@@ -0,0 +1,41 @@
+package net.kaleidos.shortener
+
+class ShortenUrl {
+
+    /**
+     * The short link (without domain)
+     */
+    String shortUrl
+
+    /**
+     * The final url
+     */
+    String targetUrl
+
+    /**
+     * The times the short url has been converted
+     */
+    Integer hits = 0
+
+    /**
+     * Creation date
+     */
+    Date dateCreated
+
+    /**
+     * Last updated
+     */
+    Date lastUpdated
+
+    static constraints = {
+        targetUrl url:true, type: "text", unique: true
+    }
+
+    static mapping = {
+        version false
+
+        // Indexes
+        email shortUrl: 'shortener_url__short_url_idx'
+        email targetUrl: 'shortener_url__target_url_idx'
+    }
+}
